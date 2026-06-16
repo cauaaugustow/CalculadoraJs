85b3c0c4df3b563111f35c63329d51cc2b3b36f8
Cauã Augusto <162489346+cauaaugustow@users.noreply.github.com>
2026-06-16 15:17:47 -0300

    feat: refatorando o codigo e mundando para cosntrutor

--- assets/script.js
+++ assets/script.js
@@ -1,75 +1,50 @@
-function criaCalculador(){
-    return {
-       display: document.querySelector('.display'),
-       btnClear: document.querySelector('.btn-clear'),
-       btnEqual: document.querySelector('.btn-eq'),
-       btnApagaUm: document.querySelector('.btn-del'),
+function Calculadora(){
+    this.display = document.querySelector('.display');
+    this.btnClear = document.querySelector('.btn-clear');
+    this.btnEqual = document.querySelector('.btn-eq');
+    this.btnApagaUm = document.querySelector('.btn-del');
 
-       equalDisplay(){
-            let conta = this.display.textContent;
+    this.equalDisplay = () =>{
+         let conta = this.display.textContent;
             try{
+                conta = conta.replace(/÷/g, '/').replace(/x/g, '*');
                 conta = eval(conta);
-                if(!conta){
-                    alert('Conta inválida');
-                    return;
-                }
-
                 this.display.textContent = conta;
             }catch(e){
                 alert('Conta inválida');
                 return;
-            }
-       },
+            };
+    };
 
-       apagaUm(){
-            this.display.textContent = this.display.textContent.slice(0, -1);
-       },
-       
-       clearDisplay() {
-            this.display.textContent = '';
-       },
-       
-        inicia() {
-            this.cliqueBotoes();
-            this.pressionaEnter();
-            
-        },
+    this.apagaUm = () => this.display.textContent = this.display.textContent.slice(0, -1);
 
-        pressionaEnter() {
-            document.addEventListener('keyup', e => {
+    this.clearDisplay = () => this.display.textContent = '';
+    this.inicia = () =>{
+        this.cliqueBotoes();
+        this.pressionaEnter();
+    };
+
+    this.pressionaEnter = () =>{
+         document.addEventListener('keyup', e => {
                 if (e.key === 'Enter') {
                     this.equalDisplay();
                 }
             });
-        },
+    };
 
-        
-        cliqueBotoes() {
-            document.querySelector('.buttons').addEventListener('click', function(e) {
+    this.cliqueBotoes = () =>{
+        document.querySelector('.buttons').addEventListener('click', function(e) {
                 const el = e.target;
-
-                if (el.classList.contains('btn-num')) {
-                    this.btnParaDisplay(el.innerText);
-                }
-                if (el.classList.contains('btn-clear')) {
-                    this.clearDisplay();
-                }
-                if (el.classList.contains('btn-del')) {
-                    this.apagaUm();
-                }
-                if (el.classList.contains('btn-eq')) {
-                    this.equalDisplay();
-                }
+                if (el.classList.contains('btn-num')) this.btnParaDisplay(el.innerText);
+                if (el.classList.contains('btn-clear')) this.clearDisplay();
+                if (el.classList.contains('btn-del')) this.apagaUm();
+                if (el.classList.contains('btn-eq')) this.equalDisplay();
 
             }.bind(this));
-        },
+    }
 
-        btnParaDisplay(valor) {
-            this.display.textContent += valor;
-        }
-
-    };
+    this.btnParaDisplay = (valor) => this.display.textContent += valor;
 }
 
-const calculadora = criaCalculador();
+const calculadora = new Calculadora();
 calculadora.inicia();
